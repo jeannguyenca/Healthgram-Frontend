adbe026d5c048e84cc1472fbc3cd558496b061ce
Jean <jeannguyen.ca@gmail.com>
2019-03-18 17:05:25 -0700

    New API key

--- src/index.js
+++ src/index.js
@@ -1,16 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./Planner/style/custom.scss";
-// import "bootstrap/dist/css/bootstrap.min.css";
-import PlannerPage from "./Planner/PlannerPage";
+import "./index.css";
+import App from "./App";
 import * as serviceWorker from "./serviceWorker";
+// import { unregister } from "./serviceWorker";
 
-// if (!global._babelPolyfill) {
-//  require('babel-polyfill');
-// }
-ReactDOM.render(<PlannerPage />, document.getElementById("root"));
+import "./style/custom.scss";
+
+ReactDOM.render(<App />, document.getElementById("root"));
+
+// unregister();
+
+serviceWorker.register();
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
